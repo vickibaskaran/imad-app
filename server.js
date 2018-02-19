be6048a8a8e5ea9c16f9ef4e4019cf7d95d333b6
vickibaskaran <34920313+vickibaskaran@users.noreply.github.com>
2018-02-19 22:43:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-   res.send('Article one is requested and it is served here') ;
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
