710c4f592adc6b79c279414f6b0663079474dd64
vickibaskaran <34920313+vickibaskaran@users.noreply.github.com>
2018-02-26 22:51:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-  var Interval=setInterval(moveRight,100);
+  var Interval=setInterval(moveRight,50);
 };
